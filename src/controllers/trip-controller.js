72a2359cc735a3c10602e1978d9d032b988255af
Otkrovennost <Otkrovennost@mail.ru>
2020-04-18 20:54:45 +0300

    вынесение отрисовки ивентов в отдельную функцию

--- src/controllers/trip-controller.js
+++ src/controllers/trip-controller.js
@@ -7,6 +7,55 @@ import EditItem from "../components/edit-item.js";
 import NoTasksComponent from "../components/no-tasks.js";
 import {sortOptions} from "../mock/sort.js";
 
+const renderTripEvents = (cards) => {
+  const datesList = [
+    ...new Set(cards.map((elem) => new Date(elem.start).toDateString()))
+  ];
+
+  const tripDaysList = document.querySelector(`.trip-days`);
+
+  datesList.forEach((date, dateIndex) => {
+
+    renderElement(tripDaysList, new Day(date, dateIndex + 1), RenderPosition.BEFOREEND);
+    const dayCurrent = tripDaysList.querySelector(`.trip-days__item:last-of-type`);
+
+    cards
+      .filter((card) => new Date(card.start).toDateString() === date)
+      .forEach((card) => {
+        const onEscKeyDown = (evt) => {
+          const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+          if (isEscKey) {
+            replaceEditToTask();
+            document.removeEventListener(`keydown`, onEscKeyDown);
+          }
+        };
+
+        const eventsList = dayCurrent.querySelector(`.trip-events__list`);
+
+        const replaceTaskToEdit = () => {
+          replace(editEventItem, newEvent);
+        };
+
+        const replaceEditToTask = () => {
+          replace(newEvent, editEventItem);
+        };
+
+        const newEvent = new DayItem(card);
+        newEvent.setClickHandler(() => {
+          replaceTaskToEdit();
+          document.addEventListener(`keydown`, onEscKeyDown);
+        });
+
+        const editEventItem = new EditItem(card);
+        editEventItem.setSubmitHandler(replaceEditToTask);
+        editEventItem.setCloseHandler(replaceEditToTask);
+
+        renderElement(eventsList, newEvent, RenderPosition.BEFOREEND);
+      });
+  });
+};
+
 export default class TripController {
   constructor(container) {
     this._container = container;
@@ -24,53 +73,7 @@ export default class TripController {
     } else {
       renderElement(container, this._sortComponent, RenderPosition.AFTERBEGIN);
       renderElement(container, this._daysList, RenderPosition.BEFOREEND);
-
-      const datesList = [
-        ...new Set(cards.map((elem) => new Date(elem.start).toDateString()))
-      ];
-
-      const tripDaysList = document.querySelector(`.trip-days`);
-
-      datesList.forEach((date, dateIndex) => {
-
-        renderElement(tripDaysList, new Day(date, dateIndex + 1), RenderPosition.BEFOREEND);
-        const dayCurrent = tripDaysList.querySelector(`.trip-days__item:last-of-type`);
-
-        cards
-          .filter((card) => new Date(card.start).toDateString() === date)
-          .forEach((card) => {
-            const onEscKeyDown = (evt) => {
-              const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-
-              if (isEscKey) {
-                replaceEditToTask();
-                document.removeEventListener(`keydown`, onEscKeyDown);
-              }
-            };
-
-            const eventsList = dayCurrent.querySelector(`.trip-events__list`);
-
-            const replaceTaskToEdit = () => {
-              replace(editEventItem, newEvent);
-            };
-
-            const replaceEditToTask = () => {
-              replace(newEvent, editEventItem);
-            };
-
-            const newEvent = new DayItem(card);
-            newEvent.setClickHandler(() => {
-              replaceTaskToEdit();
-              document.addEventListener(`keydown`, onEscKeyDown);
-            });
-
-            const editEventItem = new EditItem(card);
-            editEventItem.setSubmitHandler(replaceEditToTask);
-            editEventItem.setCloseHandler(replaceEditToTask);
-
-            renderElement(eventsList, newEvent, RenderPosition.BEFOREEND);
-          });
-      });
+      renderTripEvents(cards);
     }
   }
 }
